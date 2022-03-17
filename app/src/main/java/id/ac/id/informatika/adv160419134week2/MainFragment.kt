4e4c0d5483536a3eaa61779bd1dbb12f4db9c1cf
kiky <kikyfadhilah2001@gmail.com>
2022-03-17 18:13:10 +0700

    init

---
+++ app/src/main/java/id/ac/id/informatika/adv160419134week2/MainFragment.kt
@@ -0,0 +1,18 @@
+package id.ac.id.informatika.adv160419134week2
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class MainFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main2, container, false)
+    }
+}
